09e4b650a166a39d1efebc26f04ad614bd9a19f3
Aymane-Chahbane <chahbane.aymane@gmail.com>
2026-04-21 18:46:06 +0100

    TP 13

---
+++ src/exercice_1/SerializationUtil.java
@@ -0,0 +1,27 @@
+package exercice_1;
+
+import java.io.*;
+import java.util.List;
+
+public class SerializationUtil {
+
+   
+    public static void serializeEmployees(String path, List<Employee> data)
+            throws IOException {
+        // try-with-resources garantit la fermeture du flux
+        try (ObjectOutputStream oos =
+                     new ObjectOutputStream(new FileOutputStream(path))) {
+            oos.writeObject(data);
+        }
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    public static List<Employee> deserializeEmployees(String path)
+            throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois =
+                     new ObjectInputStream(new FileInputStream(path))) {
+            return (List<Employee>) ois.readObject();
+        }
+    }
+}
